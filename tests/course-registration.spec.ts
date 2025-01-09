bd6ae84e0cb80b3a0099df1c8e12ece28b05fa76
Chris Simon <chris@devcycles.io>
2025-01-09 16:40:34 +1100

    4. RED - ensure the course has a guid ID attribute

--- tests/course-registration.spec.ts
+++ tests/course-registration.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 
+const GUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
 test.describe('Course registration', () => {
     test('Can review list of courses',  async ({ page }) => {
         await page.goto('http://localhost:3000');
@@ -8,6 +10,9 @@ test.describe('Course registration', () => {
 
         await expect(coursesList).toBeVisible();
         await expect(coursesList.getByRole("option", { name: "Select the course you'd like to register for..." })).toBeDisabled();
-        await expect(coursesList.getByRole("option", { name: "Accessibility 101" })).toBeEnabled();
+        const accessibilityCourse = coursesList.getByRole("option", { name: "Accessibility 101" });
+        await expect(accessibilityCourse).toBeEnabled();
+        const courseId = await accessibilityCourse.getAttribute("data-course-id");
+        await expect(courseId).toMatch(GUID_REGEX);
     });
 });
