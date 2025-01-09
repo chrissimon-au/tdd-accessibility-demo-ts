b7038ee67bed055637d065b8ff456ab94f62dd85
Chris Simon <chris@devcycles.io>
2025-01-09 15:34:08 +1100

    0.1 - RED - Add axe-core test of homepage

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,8 +1,17 @@
 import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/University Registration/);
+});
+
+test('No Accessibility Issues', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
 });
