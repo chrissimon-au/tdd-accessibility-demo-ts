390faf1dedb4da03c117eeba2b62222a5233f26f
Chris Simon <chris@devcycles.io>
2025-01-09 13:19:44 +1100

    Install playwright and default config

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ APP_NAME=tdd-accessibility-demo-nextjs
 npx create-next-app@latest $APP_NAME --ts --tailwind --eslint --app --no-import-alias --src-dir --turbopack --use-npm
 mv $APP_NAME/* $APP_NAME/.[!.]* .
 rmdir $APP_NAME
+# arecord of the commands to initialise and install playwright
+TEST_OPTIONS='{"testDir":"tests"}' npm init playwright@latest -y -- --no-examples
+npx playwright install
